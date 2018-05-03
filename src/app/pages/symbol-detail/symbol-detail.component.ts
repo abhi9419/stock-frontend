65ed7dfcbffe81f6537fd38133b987ad79c225e3
Abhishek Goyal <abhishek.goyal9419@gmail.com>
2018-05-03 20:33:48 +0530

    Chart for latest 10 record

--- src/app/pages/symbol-detail/symbol-detail.component.ts
+++ src/app/pages/symbol-detail/symbol-detail.component.ts
@@ -2,6 +2,11 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpRequestService} from "../../services/http-request.service";
 
+declare const d3: any;
+declare const nv: any;
+declare const $: any;
+
+
 @Component({
     selector: 'app-symbol-detail',
     templateUrl: './symbol-detail.component.html',
@@ -14,6 +19,9 @@ export class SymbolDetailComponent implements OnInit, OnDestroy {
     latestSymbolValue = {open: 0, high: 0, low: 0, close: 0, volume: 0, average: 0};
     secondLatestSymbolValue = {open: 0, high: 0, low: 0, close: 0, volume: 0, average: 0};
     latestSymbolValueSubscriber: any;
+    latestTimestamp: string;
+    graphDataSubscriber: any;
+    graphData: string;
 
 
     constructor(private route: ActivatedRoute, private httpRequestService: HttpRequestService) {
@@ -23,14 +31,24 @@ export class SymbolDetailComponent implements OnInit, OnDestroy {
         this.currentPageSubscriber = this.route.params.subscribe(params => {
             this.currentSymbol = params['symbol'] === undefined ? '' : params['symbol'];
             this.currentSymbol = this.currentSymbol.toUpperCase();
-
             this.fetchLatesSymbolValues();
             this.fetchDataFromApiToBackend();
+            this.fetchGraphData();
         });
         this.latestSymbolValueSubscriber = this.httpRequestService.contentLoaded.subscribe(params => {
             if (params['id'] === 2) {
+                const latest = params['data']['data']['0'];
+                const secondLatest = params['data']['data']['1'];
+                this.processLatestSymbolValue(latest);
+                this.processSecondLatestSymbolValue(secondLatest);
+            }
+        });
+        this.graphDataSubscriber = this.httpRequestService.contentLoaded.subscribe(params => {
+            if (params['id'] === 3) {
                 const data = params['data']['data'];
-                this.processLatestSymbolValue(data);
+                this.graphData = JSON.stringify(data);
+                console.log(this.graphData);
+                this.n();
             }
         });
 
@@ -47,8 +65,13 @@ export class SymbolDetailComponent implements OnInit, OnDestroy {
     }
 
     fetchLatesSymbolValues() {
-        this.httpRequestService.setUrl('symbol/current/' + this.currentSymbol);
+        this.httpRequestService.setUrl('symbol/recent2/' + this.currentSymbol);
         this.httpRequestService.sendGetWithIdentifier(2);
+    }
+
+    fetchGraphData() {
+        this.httpRequestService.setUrl('symbol/graph-data/' + this.currentSymbol);
+        this.httpRequestService.sendGetWithIdentifier(3);
     }
 
     processLatestSymbolValue(data: any) {
@@ -58,6 +81,52 @@ export class SymbolDetailComponent implements OnInit, OnDestroy {
         this.latestSymbolValue.volume = data['volume'];
         this.latestSymbolValue.low = data['low'];
         this.latestSymbolValue.average = (this.latestSymbolValue.open + this.latestSymbolValue.high + this.latestSymbolValue.low + this.latestSymbolValue.close) / 4;
+        this.latestTimestamp = data['timestamp'];
+    }
+
+    processSecondLatestSymbolValue(data: any) {
+        this.secondLatestSymbolValue.open = data['open'];
+        this.secondLatestSymbolValue.high = data['high'];
+        this.secondLatestSymbolValue.close = data['close'];
+        this.secondLatestSymbolValue.volume = data['volume'];
+        this.secondLatestSymbolValue.low = data['low'];
+        this.secondLatestSymbolValue.average = (this.secondLatestSymbolValue.open + this.secondLatestSymbolValue.high + this.secondLatestSymbolValue.low + this.secondLatestSymbolValue.close) / 4;
+    }
+
+
+    n() {
+
+        nv.addGraph(function () {
+            var chart = nv.models.cumulativeLineChart()
+                .x(function (d) {
+                    return d[0]
+                })
+                .y(function (d) {
+                    return d[1]
+                })
+                .color(d3.scale.category10().range())
+                .useInteractiveGuideline(true)
+            ;
+
+            chart.xAxis
+                .tickFormat(function (d) {
+                    return d3.time.format('%x')(new Date(d))
+                })
+
+            chart.yAxis
+                .tickFormat(d3.format(''));
+
+            console.log($("#chart1").attr("data-content"));
+
+            d3.select('#chart1 svg')
+                .datum(JSON.parse($("#chart1").attr("data-content")))
+                .call(chart);
+
+
+            return chart;
+        });
+
+
     }
 
 }
--- src/assets/js/pages/dashboard.js
+++ src/assets/js/pages/dashboard.js
@@ -1,42 +1,45 @@
-$(document).ready(function() {
-    
+/*
+$(document).ready(function () {
+
     "use strict";
 
-/* Inspired by Lee Byron's test data generator. */
-function stream_layers(n, m, o) {
-  if (arguments.length < 3) o = 0;
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-        y = 2 * Math.random() - .5,
-        z = 10 / (.1 + Math.random());
-    for (var i = 0; i < m; i++) {
-      var w = (i / m - y) * z;
-      a[i] += x * Math.exp(-w * w);
-    }
-  }
-  return d3.range(n).map(function() {
-      var a = [], i;
-      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-      for (i = 0; i < 5; i++) bump(a);
-      return a.map(stream_index);
-    });
-};
+    /!* Inspired by Lee Byron's test data generator. *!/
+    function stream_layers(n, m, o) {
+        if (arguments.length < 3) o = 0;
 
-/* Another layer generator using gamma distributions. */
-function stream_waves(n, m) {
-  return d3.range(n).map(function(i) {
-    return d3.range(m).map(function(j) {
-        var x = 20 * j / m - i / 3;
-        return 2 * x * Math.exp(-.5 * x);
-      }).map(stream_index);
-    });
-};
+        function bump(a) {
+            var x = 1 / (.1 + Math.random()),
+                y = 2 * Math.random() - .5,
+                z = 10 / (.1 + Math.random());
+            for (var i = 0; i < m; i++) {
+                var w = (i / m - y) * z;
+                a[i] += x * Math.exp(-w * w);
+            }
+        }
 
-function stream_index(d, i) {
-  return {x: i, y: Math.max(0, d)};
-};
-    var nvddata1 = function() {
-        return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
+        return d3.range(n).map(function () {
+            var a = [], i;
+            for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+            for (i = 0; i < 5; i++) bump(a);
+            return a.map(stream_index);
+        });
+    };
+
+    /!* Another layer generator using gamma distributions. *!/
+    function stream_waves(n, m) {
+        return d3.range(n).map(function (i) {
+            return d3.range(m).map(function (j) {
+                var x = 20 * j / m - i / 3;
+                return 2 * x * Math.exp(-.5 * x);
+            }).map(stream_index);
+        });
+    };
+
+    function stream_index(d, i) {
+        return {x: i, y: Math.max(0, d)};
+    };
+    var nvddata1 = function () {
+        return stream_layers(3, 10 + Math.random() * 100, .1).map(function (data, i) {
             var a = i + 1;
             return {
                 key: 'Product' + a,
@@ -46,28 +49,26 @@ function stream_index(d, i) {
     };
 
 
-    nv.addGraph(function() {
-    var chart = nv.models.multiBarChart()
-    .color(['#0070E0','#637282','#F1C205'])
+    nv.addGraph(function () {
+        var chart = nv.models.multiBarChart()
+            .color(['#0070E0', '#637282', '#F1C205'])
 
-    chart.xAxis
-        .tickFormat(d3.format(',f'));
+        chart.xAxis
+            .tickFormat(d3.format(',f'));
 
-    chart.yAxis
-        .tickFormat(d3.format(',.1f'));
+        chart.yAxis
+            .tickFormat(d3.format(',.1f'));
 
-    d3.select('#chart1 svg')
-        .datum(nvddata1())
-        .transition().duration(500)
-        .call(chart)
+        d3.select('#chart1 svg')
+            .datum(nvddata1())
+            .transition().duration(500)
+            .call(chart)
         ;
 
-    nv.utils.windowResize(chart.update);
+        nv.utils.windowResize(chart.update);
 
-    return chart;
-});
-    
-    
+        return chart;
+    });
 
 
     var chart2 = function () {
@@ -77,7 +78,7 @@ function stream_index(d, i) {
 
         var data = [],
             totalPoints = 50;
-        
+
         function getRandomData() {
 
             if (data.length > 0)
@@ -109,7 +110,7 @@ function stream_index(d, i) {
             return res;
         }
 
-        var plot2 = $.plot("#chart2", [ getRandomData() ], {
+        var plot2 = $.plot("#chart2", [getRandomData()], {
             series: {
                 shadowSize: 0   // Drawing is faster without shadows
             },
@@ -149,9 +150,39 @@ function stream_index(d, i) {
     };
 
     chart2();
-    
-    
-     new Chart(document.getElementById("chart3"),{"type":"radar","data":{"labels":["Sun","Mon","Tue","Wed","Thu","Fri","Fri"],"datasets":[{"label":"My First Dataset","data":[5,9,3,7,6,5,4],"fill":true,"backgroundColor":"rgba(236, 94, 105, 0.2)","borderColor":"rgb(236, 94, 105)","pointBackgroundColor":"rgb(236, 94, 105)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(236, 94, 105)"},{"label":"My Second Dataset","data":[2,4,5,1,7,3,9],"fill":true,"backgroundColor":"rgba(0, 112, 224, 0.2)","borderColor":"rgb(0, 112, 224)","pointBackgroundColor":"rgb(0, 112, 224)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(0, 112, 224)"}]},"options":{legend: {
-            display: false
-         },"elements":{"line":{"tension":0,"borderWidth":3}}}});
+
+
+    new Chart(document.getElementById("chart3"), {
+        "type": "radar",
+        "data": {
+            "labels": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Fri"],
+            "datasets": [{
+                "label": "My First Dataset",
+                "data": [5, 9, 3, 7, 6, 5, 4],
+                "fill": true,
+                "backgroundColor": "rgba(236, 94, 105, 0.2)",
+                "borderColor": "rgb(236, 94, 105)",
+                "pointBackgroundColor": "rgb(236, 94, 105)",
+                "pointBorderColor": "#fff",
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgb(236, 94, 105)"
+            }, {
+                "label": "My Second Dataset",
+                "data": [2, 4, 5, 1, 7, 3, 9],
+                "fill": true,
+                "backgroundColor": "rgba(0, 112, 224, 0.2)",
+                "borderColor": "rgb(0, 112, 224)",
+                "pointBackgroundColor": "rgb(0, 112, 224)",
+                "pointBorderColor": "#fff",
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgb(0, 112, 224)"
+            }]
+        },
+        "options": {
+            legend: {
+                display: false
+            }, "elements": {"line": {"tension": 0, "borderWidth": 3}}
+        }
+    });
 });
+*/
